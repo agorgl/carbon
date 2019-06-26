8680f0f8cb071d0bb762b23416b2bc51135c096a
TheArtist <agorglouk@gmail.com>
2019-06-26 11:50:44 +0300

    Added glfw dependency

---
+++ deps/glfw/config.mk
@@ -0,0 +1,64 @@
+PRJTYPE = StaticLib
+VERSION = 3.3.0
+SRC = \
+	src/context.c \
+	src/init.c    \
+	src/input.c   \
+	src/monitor.c \
+	src/vulkan.c  \
+	src/window.c
+ifeq ($(TARGET_OS), Windows)
+	SRC += \
+		src/win32_init.c     \
+		src/win32_joystick.c \
+		src/win32_monitor.c  \
+		src/win32_time.c     \
+		src/win32_thread.c   \
+		src/win32_window.c   \
+		src/wgl_context.c    \
+		src/egl_context.c    \
+		src/osmesa_context.c \
+		src/null_joystick.c
+else ifeq ($(TARGET_OS), Linux)
+	SRC += \
+		src/x11_init.c       \
+		src/x11_monitor.c    \
+		src/x11_window.c     \
+		src/xkb_unicode.c    \
+		src/posix_time.c     \
+		src/posix_thread.c   \
+		src/glx_context.c    \
+		src/egl_context.c    \
+		src/osmesa_context.c \
+		src/linux_joystick.c
+else ifeq ($(TARGET_OS), Darwin)
+	SRC += \
+		src/cocoa_init.m     \
+		src/cocoa_joystick.m \
+		src/cocoa_monitor.m  \
+		src/cocoa_window.m   \
+		src/cocoa_time.c     \
+		src/posix_thread.c   \
+		src/nsgl_context.m   \
+		src/egl_context.c    \
+		src/osmesa_context.c \
+		src/null_joystick.c
+else
+	SRC += \
+		src/null_init.c      \
+		src/null_monitor.c   \
+		src/null_window.c    \
+		src/null_joystick.c  \
+		src/posix_time.c     \
+		src/posix_thread.c   \
+		src/osmesa_context.c \
+		src/null_joystick.c
+endif
+
+ifeq ($(TARGET_OS), Windows)
+	DEFINES += _GLFW_WIN32
+else ifeq ($(TARGET_OS), Linux)
+	DEFINES += _GLFW_X11
+else ifeq ($(TARGET_OS), Darwin)
+	DEFINES += _GLFW_COCOA
+endif
