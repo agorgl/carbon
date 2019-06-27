eeaa228aafefc151c9272aa4a3a37409c0b7fd89
TheArtist <agorglouk@gmail.com>
2019-06-27 15:16:04 +0300

    Engine spawns associated window instance

--- src/engine.c
+++ src/engine.c
@@ -1,9 +1,13 @@
 #include "engine.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include "mainloop.h"
+#include "window.h"
+#include "opengl.h"
 
 struct engine {
     mainloop_params ml_params;
+    window wnd;
 };
 
 engine engine_create(const engine_params* params)
@@ -13,20 +17,65 @@ engine engine_create(const engine_params* params)
     return e;
 }
 
+static void on_opengl_error(void* userdata, const char* msg)
+{
+    engine e = userdata;
+    fprintf(stderr, "%s\n", msg);
+    engine_stop(e);
+}
+
+static void on_key(window wnd, int key, int scancode, int action, int mods)
+{
+    (void)scancode; (void)mods;
+    engine e = window_get_userdata(wnd);
+    if (action == KEY_ACTION_RELEASE && key == KEY_ESCAPE)
+        engine_stop(e);
+}
+
 void engine_update(engine e, float dt)
 {
-    (void) e;
     (void) dt;
+    window_update(e->wnd);
 }
 
 void engine_render(engine e, float dt)
 {
-    (void) e;
     (void) dt;
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    window_swap_buffers(e->wnd);
 }
 
 void engine_run(engine e)
 {
+    /* Create window */
+    window wnd = window_create((window_params){
+        .title  = "Carbon",
+        .width  = 1280,
+        .height = 720,
+        .mode   = 0,
+        .ctx_params = {
+            .type    = OPENGL,
+            .version = { 3, 3 },
+            .debug   = 1
+        }
+    });
+    window_set_userdata(wnd, e);
+    window_set_callbacks(wnd, (window_callbacks){
+        .key_cb = on_key
+    });
+    e->wnd = wnd;
+
+    /* Load OpenGL extensions */
+    opengl_load_extensions(window_get_proc_address);
+
+    /* Setup OpenGL debug handler */
+    opengl_register_error_handler((opengl_error_handler_params){
+        .callback = on_opengl_error,
+        .userdata = e
+    });
+
+    /* Run main loop */
     e->ml_params = (mainloop_params){
         .update_callback = (mainloop_update_fn) engine_update,
         .render_callback = (mainloop_render_fn) engine_render,
@@ -34,6 +83,9 @@ void engine_run(engine e)
         .userdata = e
     };
     mainloop(&e->ml_params);
+
+    /* Close window */
+    window_destroy(e->wnd);
 }
 
 void engine_stop(engine e)
