2c3e15d55b70af7a8380155a6cf54ce239da6f53
TheArtist <agorglouk@gmail.com>
2019-07-06 20:47:16 +0300

    Demo creates and shows sample entity hierarchy

--- demo/src/main.c
+++ demo/src/main.c
@@ -41,6 +41,48 @@ int main(int argc, char* argv[])
     /* Create engine instance with given params */
     engine engine = engine_create(&(engine_params){});
 
+    /* Fetch the world instance */
+    ecs_world_t* world = engine_world(engine);
+
+    /* Declare transform type */
+    ecs_entity_t ecs_entity(transform) = ecs_lookup(world, "transform");
+    ecs_type_t ecs_type(transform) = ecs_type_from_entity(world, ecs_entity(transform));
+
+    /* Create parent entity */
+    ECS_ENTITY(world, p, transform);
+    ecs_set(world, p, transform, {
+        .pose = {
+            .translation = (vec3){{0.0, 0.0, 0.0}},
+            .scale = (vec3){{1.0, 1.0, 1.0}},
+            .rotation = quat_id()
+        },
+        .dirty = 1
+    });
+
+    /* Create first child entity */
+    ECS_ENTITY(world, c1, transform);
+    ecs_set(world, c1, transform, {
+        .pose = {
+            .translation = (vec3){{-3.0, 0.0, 0.0}},
+            .scale = (vec3){{0.5, 0.5, 0.5}},
+            .rotation = quat_id()
+        },
+        .dirty = 1
+    });
+    ecs_adopt(world, c1, p);
+
+    /* Create second child entity */
+    ECS_ENTITY(world, c2, transform);
+    ecs_set(world, c2, transform, {
+        .pose = {
+            .translation = (vec3){{3.0, 0.0, 0.0}},
+            .scale = (vec3){{0.3, 0.3, 0.3}},
+            .rotation = quat_id()
+        },
+        .dirty = 1
+    });
+    ecs_adopt(world, c2, p);
+
     /* Run */
     engine_run(engine);
 
