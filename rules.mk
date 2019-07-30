f45e2fc1024462281731c859665ed1fe491b9a5f
TheArtist <agorglouk@gmail.com>
2019-07-30 15:55:51 +0300

    Resources packing executes only when there are files to pack

--- rules.mk
+++ rules.mk
@@ -2,9 +2,11 @@ RES_FOLDER = res
 RES_FILES  = $(call filter-files, $(call rwildcard, $(RES_FOLDER), *))
 RES_DFILE  = res.dat
 
+ifneq ($(RES_FILES),)
 $(BUILDDIR)/$(RES_DFILE)$(OBJEXT): $(RES_FILES)
 	@$(info $(DCYAN_COLOR)[+] Wrapping$(NO_COLOR) $(DYELLOW_COLOR)$(RES_FOLDER)/$(NO_COLOR))
 	$(showcmd)tar -C $(RES_FOLDER) -cf $(basename $@) $(subst $(RES_FOLDER)/,,$(wildcard $(RES_FOLDER)/*))
 	$(showcmd)cd $(@D) && ld -r -b binary -o $(@F) $(basename $(@F))
 
 OBJ_$(D) += $(BUILDDIR)/$(RES_DFILE)$(OBJEXT)
+endif
