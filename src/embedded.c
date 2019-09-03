ed300235e2d1a70794460447d90b7a26694461d6
TheArtist <agorglouk@gmail.com>
2019-09-03 15:24:09 +0300

    Embedded data implementation now supports multiple platforms

--- src/embedded.c
+++ src/embedded.c
@@ -1,15 +1,38 @@
 #include "embedded.h"
 #include "tar.h"
 
+#ifdef __APPLE__
+#include <mach-o/getsect.h>
+#define EXTLD(NAME) \
+  extern const unsigned char _section$__DATA__ ## NAME [];
+#define LDVAR(NAME) _section$__DATA__ ## NAME
+#define LDLEN(NAME) (getsectbyname("__DATA", "__" #NAME)->size)
+#elif (defined __WIN32__) /* MinGW */
+#define EXTLD(NAME) \
+  extern const unsigned char binary_ ## NAME ## _start[]; \
+  extern const unsigned char binary_ ## NAME ## _end[];
+#define LDVAR(NAME) \
+  binary_ ## NAME ## _start
+#define LDLEN(NAME) \
+  ((binary_ ## NAME ## _end) - (binary_ ## NAME ## _start))
+#else /* GNU/Linux ld */
+#define EXTLD(NAME) \
+  extern const unsigned char _binary_ ## NAME ## _start[]; \
+  extern const unsigned char _binary_ ## NAME ## _end[];
+#define LDVAR(NAME) \
+  _binary_ ## NAME ## _start
+#define LDLEN(NAME) \
+  ((_binary_ ## NAME ## _end) - (_binary_ ## NAME ## _start))
+#endif
+
 /*-----------------------------------------------------------------
  * Embedded data
  *-----------------------------------------------------------------*/
-extern char _binary_res_dat_start[];
-extern char _binary_res_dat_end[];
+EXTLD(res_dat)
 
 int embedded_file(void** data, size_t* sz, const char* fpath)
 {
-    void* tar_data = _binary_res_dat_start;
-    size_t tar_sz  = _binary_res_dat_end - _binary_res_dat_start;
-    return tar_read_file(tar_data, tar_sz, data, sz, fpath);
+    const void* tar_data = LDVAR(res_dat);
+    const size_t tar_sz  = LDLEN(res_dat);
+    return tar_read_file((void*)tar_data, tar_sz, data, sz, fpath);
 }
