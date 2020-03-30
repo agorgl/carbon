65146522525adf470f87502f6664665497256ae4
TheArtist <agorglouk@gmail.com>
2020-03-30 21:41:18 +0300

    Initial pbr lighting implementation

--- res/shaders/pbr_light.fs.glsl
+++ res/shaders/pbr_light.fs.glsl
@@ -1,28 +1,44 @@
 #version 330
 #include <mat_params>
 #include <inc/tonemap>
+#include <inc/lighting>
 
 out vec4 fcolor;
 in vec3 vpos;
 in vec2 vtco;
 in mat3 vtbn;
 
-const vec3 light_dir = vec3(0.2, 1.0, 0.2);
-
 void main()
 {
-    // Material attributes
     vec4 bcolor = bcolor(vtco);
     vec3 normal = normal(vtco, vtbn);
     vec2 mtlrgn = mtlrgn(vtco);
 
-    // Light params
     vec3 view = normalize(view_pos - vpos);
-    vec3 ldir = normalize(light_dir);
-    vec3 lcol = vec3(1.0);
+    vec3 ldir = normalize(light_pos);
+    vec4 lcol = light_col;
 
-    float kd = max(0.0, dot(normal, ldir));
-    vec3 diffuse = kd * bcolor.rgb * lcol;
+    material mat;
+    init_material(mat);
+    mat.base_color = bcolor;
+    mat.normal     = normal;
+    mat.metallic   = mtlrgn.y;
+    mat.roughness  = mtlrgn.x;
 
-    fcolor = vec4(mix(normal, diffuse, has_bcolor_map), 1.0);
+    light light;
+    light.color       = lcol.rgb;
+    light.intensity   = lcol.a;
+    light.direction   = ldir;
+    light.attenuation = 1.0;
+
+    vec3 color = vec3(0.0);
+    pixel_params pixel;
+    get_pixel_params(mat, pixel);
+    apply_directional_light(mat, pixel, light, view, color);
+
+    color = tonemap(color);
+    color = rgb_to_srgb(color);
+    color = mix(normal, color, has_bcolor_map);
+
+    fcolor = vec4(color, 1.0);
 }
