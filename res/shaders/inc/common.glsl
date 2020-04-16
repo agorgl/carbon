27fe55aca847995eb762bfef9c08f2c829be0cbd
TheArtist <agorglouk@gmail.com>
2020-04-16 14:54:13 +0300

    Added depth encoding functions in common glsl module

--- res/shaders/inc/common.glsl
+++ res/shaders/inc/common.glsl
@@ -28,6 +28,27 @@ vec3 decode_rgbm(vec4 c)
     return c.rgb * c.rgb;
 }
 
+/**
+ * Encodes a floating point number in [0..1) range
+ * into several channels of 8 bit/channel render texture.
+ */
+vec4 encode_depth(float v)
+{
+    vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
+    enc  = fract(enc);
+    enc -= enc.yzww * vec4(1.0/255.0, 1.0/255.0, 1.0/255.0, 0.0);
+    return enc;
+}
+
+/**
+ * Decodes an 8 bit/channel value
+ * into a floating point number in [0..1) range.
+ */
+float decode_depth(vec4 rgba)
+{
+    return dot(rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0));
+}
+
 //------------------------------------------------------------------------------
 // Common debug
 //------------------------------------------------------------------------------
